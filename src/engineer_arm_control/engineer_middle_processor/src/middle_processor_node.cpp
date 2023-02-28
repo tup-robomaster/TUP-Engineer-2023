9c20236343d3327c4110e3a7513bb69a75bdfcfe
push-rush <1441488761@qq.com>
2023-02-27 22:38:49 -0500

    version_1

--- src/engineer_arm_control/engineer_middle_processor/src/middle_processor_node.cpp
+++ src/engineer_arm_control/engineer_middle_processor/src/middle_processor_node.cpp
@@ -4,38 +4,34 @@ using std::placeholders::_1;
 
 namespace middle_processor
 {
-  middle_processor_node::middle_processor_node(const rclcpp::NodeOption& options)
-  : Node("middle_processor", options)
+  middle_processor_node::middle_processor_node(const rclcpp::NodeOption &options)
+      : Node("middle_processor", options)
   {
     RCLCPP_WARN(this->get_logger(), "Starting middle_processor node...");
 
-    //订阅机械臂实际移动信息
+    // 订阅机械臂实际移动信息
     true_position_sub = this->subscription<ArmMsg>("true_arm_info", std::bind(&middle_processor_node::true_position_callback, this, _1));
-    //发布机械臂规划移动信息
+    // 发布机械臂规划移动信息
     set_position_pub = this->creat_publisher<SetMsg>("/set_arm_info", rclcpp::SensorDataQos());
     Time_start = std::chrono::steady_clock::now();
     Timer_ = this->creat_wall_timer(1000ms, std::bind(&middle_processor_node::set_callback, this));
-
   }
 
   middle_processor_node::~middle_processor_node
   {
-    
   }
 
   void middle_processor_node::true_position_callback(const global_interface::msg::set_position::ConstSharedPtr &set_arm_info)
   {
-    
   }
 
   void middle_processor_node::set_callback()
   {
     RCLCPP_INFO(this->get_logger(), "arm_setting callback ...");
-    
   }
 }
 
-int main(int argc, char** argv)
+int main(int argc, char **argv)
 {
   rclcpp::init(argc, argv);
   rclcpp::spin(std::make_shared<middle_processor::middle_processor_node>());
